b7dbcf06a1139be42ab5e6f4588c5760335958c6
Dmitry Morozov <inood@yandex.ru>
2020-11-14 12:56:01 +0300

    com2

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'У меня получилось!!'
+    return 'У меня получилось!'
 
 
 if __name__ == '__main__':
